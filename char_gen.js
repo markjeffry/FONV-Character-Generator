eaf1eedda5b839f81696490453790f2886ce73d8
Mark <jeffryfb@gmail.com>
2021-05-16 21:55:55 +0800

    Update Constant Arrays and Create Assign Special Function

--- char_gen.js
+++ char_gen.js
@@ -1,12 +1,13 @@
 const courier = {
     name: null,
     gender: null,
-    S: null,
-    P: null,
-    E: null,
-    I: null,
-    A: null,
-    L: null,
+    S: 1,
+    P: 1,
+    E: 1,
+    C: 1,
+    I: 1,
+    A: 1,
+    L: 1,
     TS1: null,
     TS2: null,
     TS3: null,
@@ -25,6 +26,11 @@ const name_array = [
     'Whisper',
     'Dragonfly',
     'Cinders'
+];
+
+const gender_array = [
+    'Male',
+    'Female'
 ];
 
 const skill_array = [
@@ -62,7 +68,66 @@ const trait_array = [
     'Wild Wasteland'
 ]
 
-courier.name = name_array[Math.floor(Math.random()*name_array.length)];
+const special_array = [
+    'S',
+    'P',
+    'E',
+    'C',
+    'I',
+    'A',
+    'L'
+]
 
-console.log(courier.name);
+//Function assigns special stats to the character
+//Minimum of 1 and Maximum of 10 for any stat
+const assign_special = char_obj => {
+    let SPECIAL = 33;
+    let stat;
+    while (SPECIAL > 0) {
+        stat = special_array[Math.floor(Math.random()*special_array.length)];
+        switch (stat) {
+            case 'S':
+                if (char_obj.S > 9) {continue;}
+                char_obj.S++;
+                break;
+
+            case 'P':
+                if (char_obj.P > 9) {continue;}
+                char_obj.P++;
+                break;
+
+            case 'E':
+                if (char_obj.E > 9) {continue;}
+                char_obj.E++;
+                break;
+
+            case 'C':
+                if (char_obj.C > 9) {continue;}
+                char_obj.C++;
+                break;
+
+            case 'I':
+                if (char_obj.I > 9) {continue;}
+                char_obj.I++;
+                break;
+
+            case 'A':
+                if (char_obj.A > 9) {continue;}
+                char_obj.A++;
+                break;
+
+            case 'L':
+                if (char_obj.L > 9) {continue;}
+                char_obj.L++;
+                break;
+        }
+        SPECIAL--;
+    }
+}
+
+courier.name = name_array[Math.floor(Math.random()*name_array.length)];
+courier.gender = gender_array[Math.floor(Math.random()*gender_array.length)];
+assign_special(courier);
+
+console.log(courier);
 
